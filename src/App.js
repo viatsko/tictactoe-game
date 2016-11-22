12ff6904bd85b201baaa93405665b6c788785e02
Vadim Ivanov <ivanov@morizo.ru>
2016-11-22 23:51:14 +0300

    Создано игровое поле

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Cell extends React.Component {
+  render() {
+    return (
+      <span className="cell"></span>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app">
+        <div className="field">
+          <Cell /> <Cell /> <Cell /> <br/>
+          <Cell /> <Cell /> <Cell /> <br/>
+          <Cell /> <Cell /> <Cell /> <br/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
