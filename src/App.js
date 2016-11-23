ed527ded02ea3cf17d1097890d4bb546a50ed22a
Vadim Ivanov <sferamail33@yandex.ru>
2016-11-23 10:59:33 +0300

    Добавлен обработчик для Cell

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Cell extends React.Component {
+  render() {
+    return (
+      <span className="cell" onClick={() => this.props.onClick()}>{this.props.value}</span>
+    );
+  }
+}
+
 class Field extends React.Component {
   constructor() {
     super();
@@ -14,21 +22,34 @@ class Field extends React.Component {
     };
   }
   
+  onCellClick(i) {
+    const cells = this.state.cells.slice();
+    cells[i] = 'X';
+    
+    this.setState({
+      cells: cells
+    });
+  }
+  
+  renderCell(i) {
+    return <Cell value={this.state.cells[i]} onClick={() => this.onCellClick(i)} />;
+  }
+  
   render() {
     return (
       <div className="field">
-        <Cell /> <Cell /> <Cell /> <br/>
-        <Cell /> <Cell /> <Cell /> <br/>
-        <Cell /> <Cell /> <Cell /> <br/>
+        {this.renderCell(0)}
+        {this.renderCell(1)}
+        {this.renderCell(2)}
+        <br />
+        {this.renderCell(3)}
+        {this.renderCell(4)}
+        {this.renderCell(5)}
+        <br />
+        {this.renderCell(6)}
+        {this.renderCell(7)}
+        {this.renderCell(8)}
       </div>
-    );
-  }
-}
-
-class Cell extends React.Component {
-  render() {
-    return (
-      <span className="cell"></span>
     );
   }
 }
